fc6f1a84f9476e0b2fca1dbb03fab28b3b7bb33b
Your Name <you@example.com>
2022-12-01 12:19:43 -0800

    My first commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
-
-
 if __name__ == "__main__":
-    arglen = len(argv) - 1
-    if arglen == 0:
-        print('0 arguments.')
-    elif arglen == 1:
-        print('1 argument:')
-        print('1: {}'.format(argv[1]))
-    else:
-        print('{} arguments:'.format(arglen))
-        for i in range(1, len(argv)):
-            print('{}: {}'.format(i, argv[i]))
+    from sys import argv
+    num = len(argv) - 1
+    print("{:d} {:s}{:s}".format(num,
+                                 "argument" if num <= 2 and num == 1
+                                 else "arguments",
+                                 "." if num == 0 else ":"))
+    for a, s in enumerate(argv):
+        if a > 0:
+            print("{}: {}".format(a, s))
