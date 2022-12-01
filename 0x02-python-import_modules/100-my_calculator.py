6720290522548b52aff21aac7bb1094a3c0c6264
Your Name <you@example.com>
2022-12-01 12:57:23 -0800

    My first commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-from sys import argv, exit
-from calculator_1 import add, sub, mul, div
-
-
 if __name__ == "__main__":
-    arglen = len(argv) - 1
-    if arglen == 0:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
         exit(1)
-    op = argv[2]
     a, b = int(argv[1]), int(argv[3])
-    if op == "+":
-        print('{} + {} = {}'.format(a, b, add(a, b)))
-    elif op == "-":
-        print('{} - {} = {}'.format(a, b, sub(a, b)))
-    elif op == "*":
-        print('{} * {} = {}'.format(a, b, mul(a, b)))
-    elif op == "/":
-        print('{} / {} = {}'.format(a, b, div(a, b)))
-    else:
-        print('Unknown operator. Available operators: +, -, * and /')
+    operator = argv[2]
+    math = {"+": add, "-": sub, "*": mul, "/": div}
+    if operator not in math:
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+    print("{:d} {} {:d} = {:d}".format(a, operator, b, math[operator](a, b)))
