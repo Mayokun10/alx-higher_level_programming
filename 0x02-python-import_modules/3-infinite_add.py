a6c498b1f8230aa7b4f3c7fa5bc797e71fa56b21
Your Name <you@example.com>
2022-12-05 06:36:17 -0800

    My first commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ != "__main__":
-    exit()
-
-argc = len(sys.argv) - 1
-
-i = 0
-result = 0
-for arg in sys.argv:
-    if i != 0:
-        result += int(arg)
-    else:
-        i += 1
-print("{:d}".format(result))
+if __name__ == "__main__":
+    from sys import argv
+    print("{}".format(sum(int(a) for a in argv[1:])))
